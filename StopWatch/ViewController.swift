100d9090b6d6b24ac4e79eab50908b841b3b0ea6
Nikolay <nickolay.i.tkachenko@gmail.com>
2020-08-06 18:07:44 +0300

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Nikolay Tkachenko on 06.08.2020.
+//  Copyright © 2020 Nikolay Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
